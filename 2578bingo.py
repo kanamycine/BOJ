4130a125758367ef28c1ec6c757c6a24c86bc1a9
kanamycine <jinyeong5320@gmail.com>
2020-09-17 01:15:58 +0900

    BOJ : 1100 하얀칸

--- 2578bingo.py
+++ 2578bingo.py
@@ -1,11 +1,34 @@
 arr = [list(map(int,input().split()))for _ in range(5)]
-print(arr)
+check = [0] * 12
+cnt = 0
 
-answer = [list(map(int,input().split()))for _ in range(5)]
+answer = []
+for _ in range(5):
+    answer += list(map(int,input().split()))
 
-for i in range(5):
-    for j in range(5):
-        for k in range(5):
-            for l in range(5):
-                if arr[i][j] == answer[k][l]:
-                    arr[i][j] == 0
+for ans in answer:
+    flag = 0
+    for i in range(5):
+        for j in range(5):
+            if arr[i][j] == ans:
+                check[i] += 1
+                check[5+j] += 1
+                if i == j:
+                    check[10] += 1
+                if i + j == 4:
+                    check[11] += 1
+                flag = 1
+                cnt += 1
+                break
+        if flag:
+            break
+    bingo = 0
+    for chk in check:
+        if chk == 5:
+            bingo +=1
+    if bingo >= 3:
+        print(cnt)
+        break
+else:
+    print(25)
+

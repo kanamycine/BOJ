5cb1b09523f5d8f97135fcd34b21278b35b17477
kanamycine <jinyeong5320@gmail.com>
2021-04-21 14:32:37 +0900

    2577_숫자의개수

---
+++ 2577_숫자의개수.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result = a * b * c
+num_list = list(map(int, str(result)))
+
+for i in range(len(str(result))):
+    arr[num_list[i]] += 1
+
+for i in range(len(arr)):
+    print(arr[i])

d74ccd2b2f2fc8c5c10468e4af52c26e4dabdde1
kanamycine <jinyeong5320@gmail.com>
2021-05-05 19:56:08 +0900

    1546_평균.success

--- 1546_평균.py
+++ 1546_평균.py
@@ -1,10 +1,10 @@
 n = int(input())
-n = int(input())
-n = int(input())
-a = [int(input()) for _ in range(n)]
-lst = []
-for i in range(n):
-    lst.append(a[i]/max(a)*100)
+lst = list(map(int, input().split()))
 
-print(sum(lst)/len(lst))
-# 웁스##
+nummax = max(lst)
+numsum = 0
+for i in range(len(lst)):
+    lst[i] = (lst[i]/nummax)*100
+    numsum += lst[i]
+
+print(numsum/n)

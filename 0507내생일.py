d47cfc193dd1b2dd1c0ff44cf68eda424cfdcd04
kanamycine <jinyeong5320@gmail.com>
2021-05-08 00:41:31 +0900

    4344.py

--- 0507내생일.py
+++ 0507내생일.py
@@ -1,17 +1,8 @@
-n = int(input())
-for i in range(n):
-    lst = list(map(int, input().split()))
-    count = 0
-    ans = 0
-    for i in range(len(lst)):
-        lst2 = lst[1:]
-    sum_lst = sum(lst2)
-    avr = sum_lst/len(lst2)
-    for i in range(len(lst2)):
-        if lst2[i] > avr:
-            count += 1
-    ans = (count/len(lst2))
-
-    ans = ans * 100
-    realans = round(ans, 3)
-    print(str(realans)+'%')
+t = int(input())
+for i in range(t):
+    scores = list(map(int, input().split()))
+    n = scores[0]
+    scores = scores[1:]
+    avg = sum(scores)/n
+    cnt = len([s for s in scores if s > avg])
+    print("%.3f" % (100*cnt/n), end='%\n')

05cd2fd06d38137e62417532a36d2b1cac243152
kanamycine <jinyeong5320@gmail.com>
2021-05-24 16:07:56 +0900

    2941.크로아티아알파벳

--- 2941.크로아티아 알파벳.py
+++ 2941.크로아티아 알파벳.py
@@ -1,13 +1,24 @@
-str = input()
-count = 0
-lst = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+# str = input()
+# count = 0
+# lst = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
+# for i in range(len(lst)):
+#     if lst[i] in str:
+#         croa = str.find(lst[i])
+#         del str[croa:len(lst[i])]
+#         count += 1
+# print(str)
+
+# res = len(str) + count
+# print(res)
+
+# 나의 풀이
+
+# 두번째 풀이
+str = input()
+lst = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 for i in range(len(lst)):
     if lst[i] in str:
-        croa = str.find(lst[i])
-        del str[croa:len(lst[i])]
-        count += 1
-print(str)
-
-res = len(str) + count
+        str = str.replace(lst[i], "0")
+res = len(str)
 print(res)

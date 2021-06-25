4f8ae28fe3a4e0451800320e326fe5638ee2430f
kanamycine <jinyeong5320@gmail.com>
2021-06-25 21:49:35 +0900

    문자열반복

--- 2675_문자열반복.py
+++ 2675_문자열반복.py
@@ -1,3 +1,14 @@
+# n = int(input())
+# arr = []
+# for j in range(n):
+#     num, str = input().split()
+#     res = ''
+#     for i in str:
+#         res += int(num) * i
+#     arr.append(res)
+# for i in arr:
+#     print(i)
+
 t = int(input())
 for i in range(t):
     num, str = input().split()

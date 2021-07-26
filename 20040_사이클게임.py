95b035d9180c21b719d85775ebcf8821dbe3a8b5
kanamycine <jinyeong5320@gmail.com>
2021-07-26 14:20:08 +0900

    TT

---
+++ 20040_사이클게임.py
@@ -0,0 +1,16 @@
+uni = []
+iput = []
+count = 0
+
+def union(start, end):
+    
+
+
+A, B= map(int(input()).split())
+for i in range(A):
+    iput.append(i)
+    uni.append(0)
+for i in range(B):
+    start , end = map(int(input().split()))
+
+

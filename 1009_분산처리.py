347f51bae305fc507aa1f388fe0cd13d522a23d7
kanamycine <jinyeong5320@gmail.com>
2021-08-07 19:30:16 +0900

    modi

--- 1009_분산처리.py
+++ 1009_분산처리.py
@@ -2,4 +2,4 @@ n = int(input())
 
 for i in range(n) :
     A, B = map(int,input().split())
-    print(str(A ** B)[-1])
+    print(str(A ** B)[-1])##
--- 10870_피보나치수5.py
+++ 10870_피보나치수5.py
@@ -4,4 +4,4 @@ for i in range(2, 21):
     lst[0] = 0
     lst[1] = 1
     lst[i] = lst[i-1] + lst[i-2]
-print(lst[n])
+print(lst[n])#
